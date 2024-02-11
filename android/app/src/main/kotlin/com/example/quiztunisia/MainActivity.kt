ba21c3542dffea43ee56f86adfa08c8b41e6e6ce
compte <stagiaire@neopolis-dev.com>
2024-02-11 13:02:16 +0100

    let's play now

---
+++ android/app/src/main/kotlin/com/example/quiztunisia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quiztunisia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/questions.dart
@@ -0,0 +1,135 @@
+import 'package:quiztunisia/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: "1.Quelle est la particularité de la Tunisie ?",
+    correctAnswerIndex: 3,
+    options: [
+      "a) C'est le seul pays africain où on parle le français",
+      "b) C'est le seul pays africain à ne pas avoir de capitale officielle",
+      "c) C'est le plus petit pays du continent africain",
+      "d) C'est le plus petit État du Maghreb",
+    ],
+  ),
+  Question(
+    question: "Quel était le premier nom de la Tunisie?",
+    correctAnswerIndex: 0,
+    options: [
+      "a) Ifriqiya",
+      "b) Tshituki",
+      "c) Tshanzi",
+      "d) Kouakoui",
+    ],
+  ),
+  Question(
+    question: "Qui a créé la première mosquée tunisienne?",
+    correctAnswerIndex: 2,
+    options: [
+      "a) Tariq ibn Ziyad",
+      "b) Abd al-Rahman",
+      "c) Oqba Ibn Nafi",
+      "d) Moussa Ibn Noçaïr",
+    ],
+  ),
+  Question(
+    question: "Dans quelle ville de Tunisie se situe le Dougga ?",
+    correctAnswerIndex: 2,
+    options: [
+      "a) Kairouan",
+      "b) Jendouba",
+      "c) Téboursouk",
+      "d) Nefza",
+    ],
+  ),
+  Question(
+    question: "Qui a construit le Colisée en Tunisie?",
+    correctAnswerIndex: 1,
+    options: [
+      "a) L'empereur Macrin",
+      "b) L'empereur Gordien",
+      "c) L'empereur Bokassa",
+      "d) L'empereur Émilien",
+    ],
+  ),
+  Question(
+    question: "En quelle année fut fondée la capitale de la Tunisie, Tunis ?",
+    correctAnswerIndex: 3,
+    options: [
+      "a) En 1944",
+      "b) En 1938",
+      "c) En 1921",
+      "d) En 1956",
+    ],
+  ),
+  Question(
+    question: "Par quel pays la Tunisie a-t-elle été colonisée en 1881 ?",
+    correctAnswerIndex: 1,
+    options: [
+      "a) Les Etas-Unis",
+      "b) La France",
+      "c) L'Angleterre",
+      "d) La Belgique",
+    ],
+  ),
+  Question(
+    question:
+        "En quelle année a été adopté définitivement le drapeau de la Tunisie ?",
+    correctAnswerIndex: 3,
+    options: [
+      "a) En 1901",
+      "b) En 1896",
+      "c) En 1924",
+      "d) En 1837",
+    ],
+  ),
+  Question(
+    question: "Quelle est la devise en Tunisie ?",
+    correctAnswerIndex: 3,
+    options: [
+      "a) L'unité dans la diversité",
+      "b) Unité, Solidarité, Développement",
+      "c) L'union fait la force",
+      "d) Liberté, Ordre, Justice",
+    ],
+  ),
+  Question(
+    question: "Quelle est la date de la fête nationale en Tunisie ?",
+    correctAnswerIndex: 3,
+    options: [
+      "a) Le 31 mai",
+      "b) Le 15 aout",
+      "c) Le 5 janvier",
+      "d) Le 20 mars",
+    ],
+  ),
+  Question(
+    question: "Quel est le deuxième nom de la mosquée Sidi Mahrez ?",
+    correctAnswerIndex: 3,
+    options: [
+      "a) Ibn Khayrun",
+      "b) Ez-Zitouna",
+      "c) Oqba Ibn Nafi",
+      "d) M'hamed Bey",
+    ],
+  ),
+  Question(
+    question: "Quel est le nom du plus grand fleuve de Tunisie ?",
+    correctAnswerIndex: 0,
+    options: [
+      "a) Le Medjerda",
+      "b) Le Nil",
+      "c) Le Moulouya",
+      "d) Le Seybouse",
+    ],
+  ),
+  Question(
+    question: "Quel est le nom du tout premier musée de Tunisie ?",
+    correctAnswerIndex: 0,
+    options: [
+      "a) Le Musée national de Carthage",
+      "b) Le Musée national du Bardo",
+      "c) Le Musée archéologique de Sousse",
+      "d) Le Musée Dar Essid",
+    ],
+  ),
+];
